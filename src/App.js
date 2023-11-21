9e0e3ab45a65d9e58a9bfa45f1180bb52705cff8
Pawel Rygiewicz <p.rygiewicz@gmail.com>
2023-11-21 23:51:47 +0100

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-  return <div className="App">your code here</div>;
+  return (
+    <div className="App">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Rewards</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default App;
